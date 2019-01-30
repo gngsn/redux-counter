4879ce09f3b36f4d86625eb568334d7e9c4f3991
tomas4540 <rudtjs4540@gmail.com>
2019-01-30 15:37:12 +0900

    Ducks -> remove actions directory and reducer file

--- src/actions/ActionTypes.js
+++ src/actions/ActionTypes.js
@@ -1,10 +1,10 @@
-/*
-    export를 해줘야 다른 파일에서 import 할 수 있음.
-    대문자로 선언하기
- */
-export const CREATE = 'CREATE';
-export const REMOVE = 'REMOVE';
-
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
-export const SET_COLOR = 'SET_COLOR';
+// /*
+//     export를 해줘야 다른 파일에서 import 할 수 있음.
+//     대문자로 선언하기
+//  */
+// export const CREATE = 'CREATE';
+// export const REMOVE = 'REMOVE';
+//
+// export const INCREMENT = 'INCREMENT';
+// export const DECREMENT = 'DECREMENT';
+// export const SET_COLOR = 'SET_COLOR';
